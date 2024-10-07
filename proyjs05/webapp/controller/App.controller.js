d260c42cb01421f9708fc4ee8dcc1da9797b5f75
Erick19 <vidal.erick@gmail.com>
2024-10-07 04:18:01 +0000

    Comment ram0507

--- proyjs05/webapp/controller/App.controller.js
+++ proyjs05/webapp/controller/App.controller.js
@@ -1,10 +1,22 @@
 sap.ui.define([
     "sap/ui/core/mvc/Controller",
-     "sap/m/MessageToast"
+     "sap/m/MessageToast",
+     "sap/ui/model/json/JSONModel"
     ]
-    ,function(Controller,MessageToast){
+    ,function(Controller,MessageToast,JSONModel){
         "use strict";
         return Controller.extend("proyjs05.Controller.App",{
+            onInit:function(){
+                var oData = {
+                    persona:{
+                        name:"Persona01"
+                    }
+                }
+                var oModel = new JSONModel(oData);
+               
+                this.getView().setModel(oModel);
+            },
+
             onHola:function(){
                 MessageToast.show("Hola EV desde controller MessageToast");
             }
