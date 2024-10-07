26575115d3917cb46e1a3a0dbb3d7f6e558320fc
Erick19 <vidal.erick@gmail.com>
2024-10-07 05:36:50 +0000

    Comment ram0509

--- proyjs05/webapp/controller/App.controller.js
+++ proyjs05/webapp/controller/App.controller.js
@@ -1,13 +1,20 @@
 sap.ui.define([
     "sap/ui/core/mvc/Controller",
      "sap/m/MessageToast",
-     "proyjs05/model/Models"
+     "proyjs05/model/Models",
+     "sap/ui/model/resource/ResourceModel"
     ]
-    ,function(Controller,MessageToast,Models){
+    ,function(Controller,MessageToast,Models,ResourceModel){
         "use strict";
         return Controller.extend("proyjs05.Controller.App",{
             onInit:function(){
+
+                //Esto da acceso al Modelo 
                 this.getView().setModel(Models.createPersona());                
+
+                // Esto estable modelo de textos
+                var i18nModel = new ResourceModel({ bundleName: "proyjs05.i18n.i18n"});
+                this.getView().setModel(i18nModel,"mo_i18n");
             },
 
             onHola:function(){
