24a5531a64d59a2ee9e7c73f20a1d302c1c9acba
Erick19 <vidal.erick@gmail.com>
2024-11-11 15:19:25 +0000

    Comment ram0703

--- proyjs05/webapp/controller/App.controller.js
+++ proyjs05/webapp/controller/App.controller.js
@@ -1,10 +1,11 @@
 sap.ui.define([
     "sap/ui/core/mvc/Controller",
      "sap/m/MessageToast",
-     "proyjs05/model/Models",
-     "sap/ui/model/resource/ResourceModel"
+     //"proyjs05/model/Models",
+     //"sap/ui/model/resource/ResourceModel",
+     "sap/ui/core/Fragment"
     ]
-    ,function(Controller,MessageToast,Models,ResourceModel){
+    ,function(Controller,MessageToast,Fragment){
         "use strict";
         return Controller.extend("proyjs05.Controller.App",{
             onInit:function(){
@@ -13,6 +14,21 @@ sap.ui.define([
 
             onHola:function(){
                 MessageToast.show("Hola EV desde controller MessageToast");
+            },
+            onHelloDialog: function(){
+                const oView = this.getView();
+
+                if(!this.byId("Id_Dialog")){
+                    Fragment.load({
+                        id:   oView.getId(),
+                        name: "proyjs05.view.HelloDialog"
+                    }).then(function(oDialog){
+                        oView.addDependent();
+                        oDialog.open();
+                    });
+                }else{
+                    this.byId("Id_Dialog").open();
+                }
             }
         });        
     });
